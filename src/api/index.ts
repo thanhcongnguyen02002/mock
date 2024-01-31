da783efb60d7ca9d2f9b0b14dc2ad0312886043e
thanhcongnguyen02002 <93530115+thanhcongnguyen02002@users.noreply.github.com>
2024-01-31 15:36:29 +0700

    ...

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,33 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: "",
-  headers: {
-    token: "",
-  },
+  baseURL: "https://65b74f5446324d531d543d91.mockapi.io/api/v1/user",
 });
+axios.interceptors.request.use(
+  function (config: any) {
+    // Do something before request is sent
+    const token = localStorage.getItem("access-token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 export default axiosClient;
